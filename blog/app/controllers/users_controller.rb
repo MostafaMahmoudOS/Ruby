9a7c89429e1027a9f8ed797cc6dae9a78b47d400
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-04-20 13:23:46 +0200

    Add part2 from part2

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :save_login_state, :only => [:new, :create] 
   def new
     @user = User.new 
   end
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
     end
     
   end
+  
   private
 
   def user_params
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root :to => "sessions#login"
+  get "signup", :to => "users#new"
+  get "login", :to => "sessions#login"
+  post "login_attempt", :to => "sessions#login_attempt"
+  get "logout", :to => "sessions#logout"
+  #get "home", :to => "sessions#home"
+  #get "profile", :to => "sessions#profile"
+  #get "setting", :to => "sessions#setting"
+  #get 'welcome/index'
   resources :articles do
     resources :comments
   end
