8eae418ce2e2c709730f5b0f999c3ef6a5c303c4
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-04-20 14:56:12 +0200

    add Authentication lab3

--- blog_lab3/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  
-  resources :articles do
-    resources :comments
-  end
-  
-  #root 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
